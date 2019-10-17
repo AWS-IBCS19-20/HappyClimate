85e93709ce9665e16ac74e9af09fd3a7ae599fbf
Sofia Guerra <sofia_guerra@m018195.local>
2019-10-16 19:34:09 -0700

    Coffee Shop Program

---
+++ SecurityBreachResponse.java
@@ -0,0 +1,5 @@
+class SecurityBreachResponse {
+  public static void main(String[] args) {
+    System.out.println("One strategy small companies could use is setting up stronger firewalls to stop security breaches from occurring in the first place. Additionally, to prevent data from being lost, companies could store data in more than one place and also frequently backup their data.");
+  }
+}
